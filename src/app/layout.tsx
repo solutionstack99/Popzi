1894e43da794e8da5dd709b766e866cfd6fa7511
solutionstack99 <solution.stack99@gmail.com>
2024-12-03 14:28:38 -0500

    Created a 3D cube and downgrade Next.js, React, ReactDom, Three.js and SLicemachine.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { repositoryName } from "@/prismicio";
 
 import "./globals.css";
 import Header from "@/components/Header";
+import ViewCanvas from "@/components/ViewCanvas";
 
 // Font files can be colocated inside of `app`
 const alpino = localFont({
@@ -23,7 +24,10 @@ export default function RootLayout({
     <html lang="en" className={alpino.variable}>
       <body className="overflow-x-hidden bg-yellow-300">
         <Header />
-        <main>{children}</main>
+        <main>
+          {children}
+          <ViewCanvas />
+        </main>
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
