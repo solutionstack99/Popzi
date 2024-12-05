ade8c18e713cf69796b95739162f9530e299708d
solutionstack99 <solution.stack99@gmail.com>
2024-12-05 14:49:48 -0500

    Added the Clouds scene.

--- src/slices/SkyDive/Scene.tsx
+++ src/slices/SkyDive/Scene.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { Cloud, Clouds, Environment, Text } from "@react-three/drei";
+import {
+  Cloud,
+  Clouds,
+  Environment,
+  OrbitControls,
+  Text,
+} from "@react-three/drei";
 import { useRef } from "react";
 import * as THREE from "three";
 
@@ -12,7 +18,7 @@ type SkyDiveProps = {
   flavor: Content.SkyDiveSliceDefaultPrimary["flavor"];
 };
 
-export default function Scene({ flavor }: SkyDiveProps) {
+export default function Scene({ flavor, sentence }: SkyDiveProps) {
   const groupRef = useRef<THREE.Group>(null);
   const canRef = useRef<THREE.Group>(null);
   const cloud1Ref = useRef<THREE.Group>(null);
@@ -34,6 +40,16 @@ export default function Scene({ flavor }: SkyDiveProps) {
           <pointLight intensity={30} color="#8C0413" decay={0.6} />
         </FloatingCan>
       </group>
+
+      {/* Clouds */}
+      <Clouds ref={cloudsRef}>
+        <Cloud ref={cloud1Ref} bounds={[10, 10, 2]} />
+        <Cloud ref={cloud2Ref} bounds={[10, 10, 2]} />
+      </Clouds>
+
+      {/* Text */}
+
+      <OrbitControls />
 
       {/* Lights */}
       <ambientLight intensity={2} color="#9DDEFA" />
