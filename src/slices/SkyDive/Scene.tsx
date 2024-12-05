946936c9713402cb03f863e0e4f6c4204d067af8
solutionstack99 <solution.stack99@gmail.com>
2024-12-05 14:36:21 -0500

    Added the Diving can scene component.

---
+++ src/slices/SkyDive/Scene.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Cloud, Clouds, Environment, Text } from "@react-three/drei";
+import { useRef } from "react";
+import * as THREE from "three";
+
+import FloatingCan from "@/components/FloatingCan";
+import { Content } from "@prismicio/client";
+
+type SkyDiveProps = {
+  sentence: string | null;
+  flavor: Content.SkyDiveSliceDefaultPrimary["flavor"];
+};
+
+export default function Scene({ flavor }: SkyDiveProps) {
+  const groupRef = useRef<THREE.Group>(null);
+  const canRef = useRef<THREE.Group>(null);
+  const cloud1Ref = useRef<THREE.Group>(null);
+  const cloud2Ref = useRef<THREE.Group>(null);
+  const cloudsRef = useRef<THREE.Group>(null);
+  const wordsRef = useRef<THREE.Group>(null);
+
+  return (
+    <group ref={groupRef}>
+      {/* Can */}
+      <group rotation={[0, 0, 0.5]}>
+        <FloatingCan
+          ref={canRef}
+          flavor={flavor}
+          rotationIntensity={0}
+          floatIntensity={3}
+          floatSpeed={3}
+        >
+          <pointLight intensity={30} color="#8C0413" decay={0.6} />
+        </FloatingCan>
+      </group>
+
+      {/* Lights */}
+      <ambientLight intensity={2} color="#9DDEFA" />
+      <Environment files="/hdr/field.hdr" environmentIntensity={1.5} />
+    </group>
+  );
+}
