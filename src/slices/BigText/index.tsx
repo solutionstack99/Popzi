c0a14f3996fe538e450a8dd10d237216d4c8ef42
solutionstack99 <solution.stack99@gmail.com>
2024-12-11 11:24:33 -0500

    Center text.

--- src/slices/BigText/index.tsx
+++ src/slices/BigText/index.tsx
@@ -17,13 +17,13 @@ const BigText = ({ slice }: BigTextProps): JSX.Element => {
       className="min-h-screen w-screen overflow-hidden bg-[#FE6334] text-[#FEE832]"
     >
       <h2 className="grid w-full gap-[3vw] py-10 text-center font-black uppercase leading-[.7]">
-        <div className="text-[34vw]">BEAR</div>
-        <div className="grid gap-[3vw] text-[34vw] md:flex md:text-[11vw]">
-          <span className="inline-block">that </span>
-          <span className="inline-block max-md:text-[27vw]">makes </span>
-          <span className="inline-block max-md:text-[40vw]">you </span>
+        <div className="text-[34vw]">LIVE RIGHT</div>
+        <div className="grid gap-[3vw] text-center text-[34vw] md:flex md:justify-center md:text-[11vw]">
+          LIVE LIGHT
+          {/* <span className="inline-block max-md:text-[27vw]">makes </span>
+          <span className="inline-block max-md:text-[40vw]">you </span> */}
         </div>
-        <div className="text-[32vw]">Smile</div>
+        {/* <div className="text-[32vw]">Smile</div> */}
       </h2>
     </section>
   );
